00ffdc85013cd778117e49fcf7189a93439759b7
masato <04masato05@gmail.com>
2024-10-14 08:54:25 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		name := c.QueryParam("name")
+		return c.String(http.StatusOK, "Hello, "+name+"!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
